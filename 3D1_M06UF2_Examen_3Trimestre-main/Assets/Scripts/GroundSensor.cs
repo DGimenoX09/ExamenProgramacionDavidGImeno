8dab98cb813dc01223fbbc8e57f6341a35483ca8
DGimenoX09 <david.gimeno.escobar@gmail.com>
2024-05-22 10:35:04 +0200

    ExamenProgramacion3r Trimestre

---
+++ 3D1_M06UF2_Examen_3Trimestre-main/Assets/Scripts/GroundSensor.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSensor : MonoBehaviour
+{
+    public bool isGrounded;
+
+    GameManager gameManager;  
+    public Animator animator; 
+
+    void Awake()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        animator = GetComponentInParent<Animator>(); 
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.layer == 3)
+        {
+            isGrounded = true;
+        }
+        else if(other.gameObject.layer == 6)
+        {
+            Debug.Log("Goomba muerto");
+            
+            Enemy goomba = other.gameObject.GetComponent<Enemy>();
+            goomba.Die();
+
+        }
+
+
+        if(other.gameObject.tag == "DeadZone")
+        {
+            Debug.Log("Estoy muerto");
+
+            gameManager.GameOver();
+        }  
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.layer == 3)
+        {
+            isGrounded = true;
+            animator.SetBool("IsJumping", false); 
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.layer == 3)
+        {
+            isGrounded = false;
+        }
+    }
+}
